01dd807ea7e44f520c2a2dc3fbe75f72c8ff11d6
Ahmed Abdullah <akdadoc@gmail.com>
2021-06-23 04:42:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ function startHttpExplorer() {
 
  function startGeth() {
     
-     exec('bin\\geth.exe --datadir="bin\\shibarium" --networkid 20210 --http --http.port 8545 --http.api "eth,net,web3, personal" --http.corsdomain "*" --bootnodes "enr:-J24QC5A7oB1syQdXddcwkgkZiRz571KHWxK-Q7hnWzbEtvQRtsZPb9nhrqPrnOFolSNeoYDL60-ir-dPFY-vDycKogCg2V0aMfGhC6e4G6AgmlkgnY0gmlwhI_GP7WJc2VjcDI1NmsxoQJyQQNS9_tLwqoMGnDQx6Rj_eovJU290FuhhgtRBQv2cYRzbmFwwIN0Y3CCndODdWRwgp3T"', (error, stdout, stderr) => {
+     exec('geth.exe --datadir="bin\\shibarium" --networkid 20210 --http --http.port 8545 --http.api "eth,net,web3, personal" --http.corsdomain "*" --bootnodes "enr:-J24QC5A7oB1syQdXddcwkgkZiRz571KHWxK-Q7hnWzbEtvQRtsZPb9nhrqPrnOFolSNeoYDL60-ir-dPFY-vDycKogCg2V0aMfGhC6e4G6AgmlkgnY0gmlwhI_GP7WJc2VjcDI1NmsxoQJyQQNS9_tLwqoMGnDQx6Rj_eovJU290FuhhgtRBQv2cYRzbmFwwIN0Y3CCndODdWRwgp3T"', (error, stdout, stderr) => {
         if (error) {
             console.log(`<error: ${error.message}`)
             return false
